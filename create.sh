e1b7ec0dd4c3c3b33e9e7f7abaf819db00a7d9fb
walzph <walz.ph@gmail.com>
2021-07-19 09:53:09 +0200

    Init custom helm library

---
+++ create.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+touch sandbox-authorisation/templates/deployment.yaml
+touch sandbox-notification/templates/deployment.yaml
+touch sandbox-central/templates/deployment.yaml
+touch sandbox-request/templates/deployment.yaml
+touch sandbox-metadata/templates/deployment.yaml
+touch sandbox-ui/templates/deployment.yaml
